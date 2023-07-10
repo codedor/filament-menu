119e4d08f7327bee03763cbe20824add5f0f4c11
Louise Devolder <louise.devolder@onlyhumans.com>
2023-07-10 12:42:31 +0000

    add styling menu items

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -16,51 +16,79 @@
 >
     <div class="relative group">
         <div @class([
-            'bg-white rounded-lg border border-gray-300 w-full flex',
-            'dark:bg-gray-700 dark:border-gray-600' => config('filament.dark_mode'),
-        ])>
+            'bg-white rounded-lg border border-gray-300 w-full flex menu-item',
+            'dark:bg-gray-700 dark:border-gray-600' => config('filament.dark_mode')
+        ])
+        >
             <button type="button" @class([
-                'flex items-center bg-gray-50 rounded-l-lg border-r border-gray-300 px-px',
+                'flex items-center bg-gray-50 rounded-l-lg border-r border-gray-300 p-2 cursor-grab focus:cursor-grabbing',
                 'dark:bg-gray-800 dark:border-gray-600' => config('filament.dark_mode'),
             ]) data-sortable-handle>
                 @svg('heroicon-o-dots-vertical', 'text-gray-400 w-4 h-4 -mr-2')
                 @svg('heroicon-o-dots-vertical', 'text-gray-400 w-4 h-4')
             </button>
 
+            <span class="py-2 px-4">
+                <span>{{ $item->working_title }}</span>
 
-            <span>{{ $item->working_title }}</span>
-
-            @if(count($item->children) > 0)
-                <button type="button" x-on:click="open = !open" title="Toggle children" class="appearance-none text-gray-500">
-                    <svg class="w-3.5 h-3.5 transition ease-in-out duration-200" x-bind:class="{
-                        '-rotate-90': !open,
-                    }" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </button>
-            @endif
+                @if(count($item->children) > 0)
+                    <button
+                        type="button"
+                        x-on:click="open = !open"
+                        title="Toggle children"
+                        class="appearance-none text-gray-500"
+                    >
+                        <svg class="w-3.5 h-3.5 transition ease-in-out duration-200"
+                            x-bind:class="{
+                                '-rotate-90': !open,
+                            }"
+                            fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                @endif
+            </span>
         </div>
 
         <div @class([
-            'absolute top-0 right-0 h-6 divide-x rounded-bl-lg rounded-tr-lg border-gray-300 border-b border-l overflow-hidden rtl:border-l-0 rtl:border-r rtl:right-auto rtl:left-0 rtl:rounded-bl-none rtl:rounded-br-lg rtl:rounded-tr-none rtl:rounded-tl-lg hidden opacity-0 group-hover:opacity-100 group-hover:flex transition ease-in-out duration-250',
+            'menu-item--buttons absolute right-3 flex items-center',
             'dark:border-gray-600 dark:divide-gray-600' => config('filament.dark_mode'),
         ])>
+            <button
+                x-on:click="openEditModal({{ $item->id }})"
+                class="
+                    filament-link
+                    inline-flex items-center justify-center gap-0.5
+                    font-medium text-sm text-primary-600 hover:text-primary-500
+                    hover:underline focus:underline
+                    filament-tables-link-action mr-4
+                "
+            >
+                <x-heroicon-o-pencil class="filament-link-icon w-4 h-4 mr-1 rtl:ml-1" />
+                Edit
+            </button>
             <button
                 x-init
                 x-tooltip.raw.duration.0="{{__('filament-navigation::filament-navigation.items.remove')}}"
                 type="button"
                 wire:click="removeItem('{{ $item->id }}')"
-                class="p-1"
+                class="
+                    filament-link
+                    inline-flex items-center justify-center gap-0.5
+                    font-medium text-sm text-danger-600 hover:text-danger-500
+                    hover:underline focus:underline
+                    filament-tables-link-action
+                "
                 title="{{__('filament-navigation::filament-navigation.items.remove')}}"
             >
-                <x-heroicon-o-trash class="w-3 h-3 text-danger-500 hover:text-danger-900" />
-            </button>
-
-            <button x-on:click="openEditModal({{ $item->id }})">
-                Edit
+                <x-heroicon-o-trash class="filament-link-icon w-4 h-4 mr-1 rtl:ml-1" />
+                Delete
             </button>
         </div>
     </div>
 
-    <div x-show="open" x-collapse class="ml-6">
+    <div x-show="open" x-collapse class="menu-child-list">
         <div
             class="space-y-2"
             wire:key="{{ $statePath }}-children"
